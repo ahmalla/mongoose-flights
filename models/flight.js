1af909ef767c30dccac8365887516c80b1a34f2c
ahmalla <ahmalla@hotmail.com>
2022-10-31 15:58:58 -0700

    struggling to write create function

--- models/flight.js
+++ models/flight.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 	
 const flightSchema = new Schema({
-  airplane: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date,
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date,
+  }, 
 });
+
+module.exports = mongoose.model('Flight', flightSchema);
