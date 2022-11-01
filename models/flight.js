75aededd5f666adb5394a8c86b3e161ad0d70b2d
ahmalla <ahmalla@hotmail.com>
2022-11-01 12:02:28 -0700

    part 1 complete

--- models/flight.js
+++ models/flight.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 	
 const flightSchema = new Schema({
-  Airline: { type: String, enum: ['American', 'Southwest', 'United'] },
-  Airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'] },
-  FlightNo: { type: Number, min: 10, max: 9999 },
-  Departs: { type: Date }, 
+  airline: { type: String, enum: ['American', 'Southwest', 'United'] },
+  airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'] },
+  flightNo: { type: Number, min: 10, max: 9999 },
+  departs: { type: Date }, 
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
--- routes/flights.js
+++ routes/flights.js
@@ -4,8 +4,9 @@ var flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
 
+router.get('/', flightsCtrl.index);
 router.get('/new', flightsCtrl.new);
 router.post('/', flightsCtrl.create);
-router.get('/', flightsCtrl.index);
+
 
 module.exports = router;
