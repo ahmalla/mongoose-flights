8af3858e0f83c5acb3aa12d4a783111a62bf331b
ahmalla <ahmalla@hotmail.com>
2022-11-01 15:51:16 -0700

    add destination section added

--- models/flight.js
+++ models/flight.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
+const destinationSchema = new Schema({
+  airport: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+  arrival: Date
+});
 	
 const flightSchema = new Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
   airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'] },
   flightNo: { type: Number, min: 10, max: 9999 },
-  departs: { type: Date }, 
+  departs: { type: Date },
+  destination: [destinationSchema] 
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
